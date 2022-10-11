cfb4ac3de0bd2ff6b3206aeaa0b4073824176fd2
JackNdigara <peterjackson.ndigara@student.moringaschool.com>
2022-10-11 23:31:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+//import { faSharp } from "@fortawesome/free-solid-svg-icons";
+//export default function Home () {
+  //  return(
+    //    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      //      <FontAwesomeIcon icon="fa-sharp fa-solid fa-money-check-dollar-pen" />
+        //</div>
+    //);
+//}
+
 ReactDOM.render(<App />, document.getElementById('root'));
